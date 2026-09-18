6c0a59a623845475a5c73aafed53b214ca3c7bc8
perorena <taki.pero.5921@gmail.com>
2026-09-18 13:15:24 +0900

    Add files via upload

--- HitAndBlow.js
+++ HitAndBlow.js
@@ -1,5 +1,4 @@
-const OuterThickness = 2;
-const DivisionThickness = 7;
+const OuterThickness = 1;
 const NumberMojiSize = '35';
 const IdSeparator = '#';
 const CellWidth = 50;
@@ -69,6 +68,7 @@ function makeTable(parentId){
         let select = document.createElement('select');
         select.setAttribute('id', inputId);
         select.setAttribute('class', 'input'); // CSSクラスはそのまま流用可能
+        select.style.fontSize = (NumberMojiSize -10).toString() + 'px';
         // 初期値（空選択肢）を追加
         let defaultOption = document.createElement('option');
         defaultOption.value = '';
